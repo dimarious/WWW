e4c5666deee8e9558c98a49a4fac5b2f667f400c
dimarious <89578866+dimarious@users.noreply.github.com>
2021-09-11 10:01:17 +0200

    Add files via upload

---
+++ first website/main.js
@@ -0,0 +1,15 @@
+console.log ("JS sagt HI!");
+// Вывод в одной строке
+document.writeln("<h2>Тame: Dimitri<h2>");
+// writeln - будет выведен с пропуском строки
+// console.log("Какая-либо информация");
+document.writeln("<h3>Surname:Dimarious<h3>");
+// Вывод информации в консоль
+// console.log("Какая-либо информация");
+var l1 = 2020;
+var l2 = 1952;
+var l2 = l1 - l2;
+document.write("Мне ", l2, " лет.", "<br/>");
+var a=5;
+a--;
+console.log(a+2);
